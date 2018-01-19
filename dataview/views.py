b8c99f620218cc53c11202bb511987bebc11b152
zwj4386 <31980852+zwj4386@users.noreply.github.com>
2018-01-19 12:06:02 +0800

    Update views.py

--- dataview/views.py
+++ dataview/views.py
@@ -34,64 +34,7 @@ def home(request):
         j = j[0]
         cl.append(j)
 
-    yr = YqsjRecord.objects
-    gjz = yr.all().values_list("gjz")
-    xw = yr.all().values_list("sjxwnum")
-    xwjg = yr.all().values_list("xwjgnum")
-    tb = yr.all().values_list("sjtbnum")
-    tbjg = yr.all().values_list("tbjgnum")
-    wx = yr.all().values_list("sjwxnum")
-    wxjg = yr.all().values_list("wxjgnum")
-    gl = []
-    xl = []
-    xjl = []
-    tl = []
-    tjl = []
-    wl = []
-    wjl = []
-    for wj in wxjg:
-        wj = wj[0]
-        wjl.append(wj)
-
-    for w in wx:
-        w = w[0]
-        wl.append(w)
-
-    for tj in tbjg:
-        tj = tj[0]
-        tjl.append(tj)
-
-    for t in tb:
-        t = t[0]
-        tl.append(t)
-
-    for xj in xwjg:
-        xj = xj[0]
-        xjl.append(xj)
-
-    for x in xw:
-        x = x[0]
-        xl.append(x)
-
-    for g in gjz:
-        g = ''.join(g).encode('utf-8')
-        gl.append(g)
-
-    tp = TrsPc.objects
-    t_time = tp.all().values_list("time")
-    t_count = tp.all().values_list("count")
-    tList = []
-    tCount = []
-    for t in t_time:
-        t = ''.join(t).encode('utf-8')
-        tList.append(t)
-
-    for c in t_count:
-        c = c[0]
-        tCount.append(c)
 
     return render(request, 'home.html',
-                  {'rl': json.dumps(rl), 'cl': json.dumps(cl) ,'xl': json.dumps(xl), 'xjl': json.dumps(xjl),
-                   'tl': json.dumps(tl), 'tjl': json.dumps(tjl), 'wl':json.dumps(wl),'wjl':json.dumps(wjl),
-                   'gl':json.dumps(gl), 'tList': tList, 'tCount':tCount,'cjmc':json.dumps(cjmc),'ttlist':ttlist,'inlist':inlist})
+                  { 'tList': tList, 'tCount':tCount,'cjmc':json.dumps(cjmc),'ttlist':ttlist,'inlist':inlist})
 
